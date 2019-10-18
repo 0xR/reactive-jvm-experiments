52dea3424863a7a918c574022046a3e0e6f45563
Jeroen Rosenberg <jrosenberg@xebia.com>
2019-10-18 14:07:42 +0200

    Cleanup

--- akka-streams-tcp/src/main/scala/com/github/jeroenr/Boot.scala
+++ akka-streams-tcp/src/main/scala/com/github/jeroenr/Boot.scala
@@ -2,11 +2,9 @@ package com.github.jeroenr
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Tcp.{IncomingConnection, ServerBinding}
-import akka.stream.scaladsl.{Flow, Framing, Sink, Source, Tcp}
+import akka.stream.scaladsl.{Flow, Framing, Sink, Tcp}
 import akka.util.ByteString
 
-import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
 
@@ -15,26 +13,25 @@ object Boot extends App {
   implicit val mat = ActorMaterializer()
   implicit val ec = system.dispatcher
 
-  private val outgoing = Tcp().outgoingConnection(
-    "localhost", 8080
-  )
+  Tcp()
+    .bind("0.0.0.0", 1337)
+    .runForeach { connection =>
+      println(s"New connection from: ${connection.remoteAddress}")
 
-  val connections: Source[IncomingConnection, Future[ServerBinding]] =
-    Tcp().bind("0.0.0.0", 1337)
-  connections.runForeach { connection =>
-    println(s"New connection from: ${connection.remoteAddress}")
+      val outgoing = Tcp().outgoingConnection(
+        "localhost", 8080
+      )
 
-    val pipeFlow = Flow[ByteString]
-      .via(Framing.delimiter(ByteString("\n"), maximumFrameLength = 256, allowTruncation = true))
-      .map(_.utf8String)
-      .alsoTo(Sink.foreach(println))
+      val pipeFlow = Flow[ByteString]
+        .via(Framing.delimiter(ByteString("\n"), maximumFrameLength = 256, allowTruncation = true))
+        .map(_.utf8String)
+        .alsoTo(Sink.foreach(println))
         .map(ByteString.apply)
-        .alsoTo(outgoing.to(Sink.ignore))
-    connection.handleWith(pipeFlow)
-  }.onComplete {
+        .via(outgoing)
+
+      connection.handleWith(pipeFlow)
+    }.onComplete {
     case Success(_) => println("Done!")
     case Failure(t) => println(s"Boom! $t")
   }
-
-
 }
