af5defdb72924e6cade21b8419d668804811d146
Matthisk Heimensen <m@tthisk.nl>
2019-10-18 10:36:38 +0200

    Added reactor tcp server

--- reactor-experiment/src/main/kotlin/Application.kt
+++ reactor-experiment/src/main/kotlin/Application.kt
@@ -1,5 +1,18 @@
-class Application {
-    fun main(args: String) {
-        println("hello world")
-    }
+import reactor.netty.tcp.TcpServer
+import java.nio.charset.Charset
+
+fun main(args: Array<String>) {
+    val server = TcpServer.create()
+        .host("localhost")
+        .port(8080)
+        .handle { inbound, _ -> inbound
+            .receive()
+            .map({ it.toString(Charset.defaultCharset()) })
+            .log()
+            .then()
+        }
+        .bindNow()
+
+    server.onDispose()
+        .block();
 }
