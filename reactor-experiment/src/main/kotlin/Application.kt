64664e24f58a46634d677765bc943bc70a068c4d
Jeroen Rosenberg <jrosenberg@xebia.com>
2019-10-18 11:17:38 +0200

    Back pressure dingen..

--- reactor-experiment/src/main/kotlin/Application.kt
+++ reactor-experiment/src/main/kotlin/Application.kt
@@ -1,15 +1,31 @@
+import reactor.core.publisher.Flux
+import reactor.core.scheduler.Schedulers.elastic
 import reactor.netty.tcp.TcpServer
 import java.nio.charset.Charset
+import java.time.Duration
+import kotlin.random.Random
 
 fun main(args: Array<String>) {
     val server = TcpServer.create()
         .host("localhost")
         .port(8080)
-        .handle { inbound, _ -> inbound
-            .receive()
-            .map({ it.toString(Charset.defaultCharset()) })
-            .log()
-            .then()
+        .handle { inbound, outbound ->
+            inbound
+                .receive()
+                .map { it.toString(Charset.defaultCharset()) }
+                .zipWith(Flux
+                    .interval(Duration.ofMillis(200L))
+                    .map { Random.nextInt(10) })
+                .groupBy { it.t2 }
+                .flatMap { it ->
+                    outbound.sendString(it
+                        .take(Duration.ofSeconds(1L))
+                        .count()
+                        .map { "Processed $it elements/sec\n" }
+                    ).then()
+                }
+                .subscribeOn(elastic())
+                .then()
         }
         .bindNow()
 
