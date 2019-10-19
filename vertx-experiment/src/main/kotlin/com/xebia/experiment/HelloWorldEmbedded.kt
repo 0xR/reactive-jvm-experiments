3191193c19fe15f0ad6d28144cb159f584603142
Ruben Oostinga <ruben.oostinga@gmail.com>
2019-10-19 18:26:28 +0200

    Add clustering

--- vertx-experiment/src/main/kotlin/com/xebia/experiment/HelloWorldEmbedded.kt
+++ vertx-experiment/src/main/kotlin/com/xebia/experiment/HelloWorldEmbedded.kt
@@ -1,6 +1,7 @@
 package com.xebia.experiment;
 
 import io.vertx.core.Vertx
+import io.vertx.core.VertxOptions
 import io.vertx.core.buffer.Buffer
 import io.vertx.ext.reactivestreams.ReactiveWriteStream
 import io.vertx.ext.web.Router
@@ -8,50 +9,20 @@ import io.vertx.kotlin.core.closeAwait
 import io.vertx.kotlin.core.http.listenAwait
 import io.vertx.kotlin.core.net.listenAwait
 import reactor.core.publisher.Flux
-import reactor.core.publisher.toMono
 import java.net.BindException
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter.ISO_LOCAL_TIME
-import java.util.function.BiFunction
 import kotlin.time.ExperimentalTime
 import kotlin.time.milliseconds
 import kotlin.time.seconds
 import kotlin.time.toJavaDuration
+import io.vertx.kotlin.core.Vertx as VertxKt
 
 
 @ExperimentalTime
 suspend fun main() {
-    val vertx = Vertx.vertx()
+    val vertx = VertxKt.clusteredVertxAwait(VertxOptions());
 
-    val router = Router.router(vertx);
-
-    router.route().path("/").handler { routingContext ->
-        // This handler will be called for every request
-        val response = routingContext.response();
-        response.putHeader("content-type", "text/plain");
-
-        // Write to the response and end it
-        response.end("Hello World from vertx");
-    };
-
-    val port = 8080
-    try {
-        vertx
-            .createHttpServer()
-            .requestHandler(router)
-            .listenAwait(port)
-
-        println("Listening http://localhost:$port/")
-    } catch (e: BindException) {
-        System.err.println("Failed to listen on port $port")
-        vertx.closeAwait()
-    }
-
-    tcpServer(vertx);
-}
-
-@ExperimentalTime
-suspend fun tcpServer(vertx: Vertx) {
     val server = vertx.createNetServer();
     server.connectHandler { socket ->
         val writeStream = ReactiveWriteStream.writeStream<Buffer>(vertx)
@@ -74,5 +45,25 @@ suspend fun tcpServer(vertx: Vertx) {
             subscription.dispose()
         }
     };
-    server.listenAwait(8090, "localhost");
+
+    val netServer = server.listenAwait(0, "localhost")
+    println("Listening on tcp://localhost:${netServer.actualPort()}")
+
+
+    val eventBus = vertx.eventBus()
+
+    val publishAdress = "publish.all"
+    eventBus.consumer<String>(publishAdress).handler {
+        println("EventBus handler got: ${it.body()}")
+    }
+    eventBus.publish(publishAdress, "Hi all!")
+//
+//    eventBus.consumer<String>()
+//
+//    ReactiveWriteStream
+//    ReadStream
+//
+//    Flux.interval(1.seconds.toJavaDuration())
+//
+//    Pump.pump
 }
