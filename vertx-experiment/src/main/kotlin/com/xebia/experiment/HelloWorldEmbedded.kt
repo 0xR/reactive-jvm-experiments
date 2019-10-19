0b381722e802c904d5a638d1c13feeee0f6e3c14
Ruben Oostinga <ruben.oostinga@gmail.com>
2019-10-19 18:55:14 +0200

    Send tcp input over eventbus

--- vertx-experiment/src/main/kotlin/com/xebia/experiment/HelloWorldEmbedded.kt
+++ vertx-experiment/src/main/kotlin/com/xebia/experiment/HelloWorldEmbedded.kt
@@ -1,17 +1,13 @@
 package com.xebia.experiment;
 
-import io.vertx.core.Vertx
 import io.vertx.core.VertxOptions
 import io.vertx.core.buffer.Buffer
+import io.vertx.ext.reactivestreams.ReactiveReadStream
 import io.vertx.ext.reactivestreams.ReactiveWriteStream
-import io.vertx.ext.web.Router
-import io.vertx.kotlin.core.closeAwait
-import io.vertx.kotlin.core.http.listenAwait
 import io.vertx.kotlin.core.net.listenAwait
 import reactor.core.publisher.Flux
-import java.net.BindException
 import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter.ISO_LOCAL_TIME
+import java.time.format.DateTimeFormatter
 import kotlin.time.ExperimentalTime
 import kotlin.time.milliseconds
 import kotlin.time.seconds
@@ -23,47 +19,39 @@ import io.vertx.kotlin.core.Vertx as VertxKt
 suspend fun main() {
     val vertx = VertxKt.clusteredVertxAwait(VertxOptions());
 
-    val server = vertx.createNetServer();
-    server.connectHandler { socket ->
-        val writeStream = ReactiveWriteStream.writeStream<Buffer>(vertx)
-        socket.pipeTo(writeStream)
-        val writeStreamFlux = Flux.from(writeStream)
-
-        val shouldDelayFlux = Flux.interval(3.seconds.toJavaDuration()).map {
-            it % 2 == 0L
-        }.startWith(false)
-
-        val subscription = shouldDelayFlux.switchMap { shouldDelay ->
-            if (shouldDelay) writeStreamFlux.delayElements(1000.milliseconds.toJavaDuration()) else writeStreamFlux
-        }
-            .subscribe {
-                val timeString = LocalDateTime.now().format(ISO_LOCAL_TIME)
-                println("[$timeString] Got ${it.length()} bytes ")
-            }
-
-        socket.closeHandler {
-            subscription.dispose()
-        }
-    };
-
-    val netServer = server.listenAwait(0, "localhost")
-    println("Listening on tcp://localhost:${netServer.actualPort()}")
-
-
     val eventBus = vertx.eventBus()
 
     val publishAdress = "publish.all"
-    eventBus.consumer<String>(publishAdress).handler {
-        println("EventBus handler got: ${it.body()}")
+    eventBus.consumer<Buffer>(publishAdress).handler {
+        val timeString = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME)
+        println("[$timeString] EventBus handler got buffer length: ${it.body().length()}")
     }
-    eventBus.publish(publishAdress, "Hi all!")
-//
-//    eventBus.consumer<String>()
-//
-//    ReactiveWriteStream
-//    ReadStream
-//
-//    Flux.interval(1.seconds.toJavaDuration())
-//
-//    Pump.pump
+    val eventBusPublisher = eventBus.publisher<Buffer>(publishAdress)
+
+    val server = vertx.createNetServer();
+    server.connectHandler { socket ->
+        val reactiveWriteStream = ReactiveWriteStream.writeStream<Buffer>(vertx)
+        socket.pipeTo(reactiveWriteStream)
+        val writeStreamFlux = Flux.from(reactiveWriteStream)
+
+        val reactiveReadStream = ReactiveReadStream.readStream<Buffer>()
+
+        val shouldDelayFlux = Flux.interval(10.seconds.toJavaDuration()).map {
+            it % 2 == 0L
+        }.startWith(false)
+
+        shouldDelayFlux.switchMap { shouldDelay ->
+            if (shouldDelay) writeStreamFlux.delayElements(1000.milliseconds.toJavaDuration()) else writeStreamFlux
+        }
+            .subscribe(reactiveReadStream)
+
+        socket.closeHandler {
+            reactiveReadStream.onComplete()
+        }
+
+        reactiveReadStream.pipeTo(eventBusPublisher)
+    };
+
+    val netServer = server.listenAwait(8090, "localhost")
+    println("Listening on tcp://localhost:${netServer.actualPort()}")
 }
